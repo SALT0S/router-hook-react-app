59d140f315fd7181da40dde7cd6c45339beceb96
salt0s <josesansalt@gmail.com>
2021-07-21 22:37:44 -0500

    Adding Api

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Nosotros = () => {
+	const [equipo, setEquipo] = useState([]);
+
+	useEffect(() => {
+		obtenerDatos();
+	}, []);
+
+	const obtenerDatos = async () => {
+		const data = await fetch('https://jsonplaceholder.typicode.com/users');
+		const users = await data.json();
+		setEquipo(users);
+	};
+
 	return (
 		<div>
 			<h1>Nosotros</h1>
+
+			<ul>
+				{equipo.map(item => (
+					<li key={item.id}>
+						{item.name} - {item.email}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
